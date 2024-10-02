d0ed9d111bb4ce97315cf722980bec98ef042717
JyojiTenguam <j.y.oji@hotmail.com>
2024-10-02 02:57:04 -0300

    Resolvendo requisito 02 de Project Algorithms

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('trybe', 'trybe')
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(3, 3)
+
+    assert encrypt_message('trybe', -1) == 'ebyrt'
+    assert encrypt_message('trybe', 3) == 'yrt_eb'
+    assert encrypt_message('trybe', 2) == 'eby_rt'
+    assert encrypt_message('trybe', 1) == 't_ebyr'
+    assert encrypt_message('trybe', 4) == 'e_byrt'
+    assert encrypt_message('trybe', 5) == 'ebyrt'
